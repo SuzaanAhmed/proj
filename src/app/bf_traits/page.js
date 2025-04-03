14252456bb1dbf550fcfc42c0110a4ad7a649603
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-04-03 16:32:07 +0530

    Finishing touch

---
+++ src/app/bf_traits/page.js
@@ -0,0 +1,14 @@
+export default function BfTraits() {
+    return (
+      <div>
+        <h1 className="text-4xl font-bold text-center sm:text-left">Me as your Boyfriend</h1>
+        <ol className="list-disc ml-6 mt-2">
+          <li>Always there for you</li>
+          <li>Supportive</li>
+          <li>Cooks for you</li>
+          <li>Cherish you like a queen</li>
+        </ol>
+      </div>
+    );
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import BfTraits from "./bf_traits/page";
+import Header from "./header/page";
 
 export default function Home() {
   return (
@@ -76,18 +78,3 @@ function Baby() {
     </div>
   );
 }
-
-function BfTraits() {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold text-center sm:text-left">Me as your Boyfriend</h1>
-      <ol className="list-disc ml-6 mt-2">
-        <li>Always there for you</li>
-        <li>Supportive</li>
-        <li>Cooks for you</li>
-        <li>Cherish you like a queen</li>
-      </ol>
-    </div>
-  );
-}
-
