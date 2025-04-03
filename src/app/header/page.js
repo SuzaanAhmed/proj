1b070a5058433ec34e2f8008550f8a6efc9927f9
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-04-03 16:30:52 +0530

    Finishing touch

---
+++ src/app/header/page.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+      <div>
+        <header className="flex justify-between items-center gap-8">
+          <Image src="/bhary.jpg" alt="Image" width={200} height={200} />
+        </header>
+        <b>Bharath</b><br />
+        <b>Skills: Smart, Handsome, Genius, Cutie</b><br />
+        <b>Flaws: Viciously Attractive; once you know me, you cannot live without me</b><br />
+      </div>
+    );
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -91,15 +91,3 @@ function BfTraits() {
   );
 }
 
-function Header() {
-  return (
-    <div>
-      <header className="flex justify-between items-center gap-8">
-        <Image src="/bhary.jpg" alt="Image" width={200} height={200} />
-      </header>
-      <b>Bharath</b><br />
-      <b>Skills: Smart, Handsome, Genius, Cutie</b><br />
-      <b>Flaws: Viciously Attractive; once you know me, you cannot live without me</b><br />
-    </div>
-  );
-}
